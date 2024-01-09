f5dd4c146cba63947365e302b1b1aaf3ea8c650f
clementIliou <clement.iliou@epita.fr>
2024-01-09 18:24:28 +0100

    testé c douté

--- backendArchi/src/app.service.ts
+++ backendArchi/src/app.service.ts
@@ -3,15 +3,48 @@ import { Pin, Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class AppService {
-  prisma = new PrismaClient();
+  master = new PrismaClient();
+  //declare second prisma client linked to the slave
+  slave = new PrismaClient({
+    datasources: {
+      db: {
+        url: 'postgresql://adminuser:password@pgslavearchi.westeurope.cloudapp.azure.com:5432/DATABASE',
+      },
+    },
+  });
   getHello(): string {
     return `Hello`;
   }
 
+  randInt = Math.floor(Math.random() * 2);
+
   async pin(
     pinWhereUniqueInput: Prisma.PinWhereUniqueInput,
   ): Promise<Pin | null> {
-    return this.prisma.pin.findUnique({ where: pinWhereUniqueInput });
+    const randInt = Math.floor(Math.random() * 2);
+    if (randInt === 0) {
+      //try master first
+      try {
+        return this.master.pin.findUnique({ where: pinWhereUniqueInput });
+      } catch (error) {
+        try {
+          return this.slave.pin.findUnique({ where: pinWhereUniqueInput });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    } else {
+      // try slave first
+      try {
+        return this.slave.pin.findUnique({ where: pinWhereUniqueInput });
+      } catch (error) {
+        try {
+          return this.master.pin.findUnique({ where: pinWhereUniqueInput });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
   }
 
   async pins(params: {
@@ -22,7 +55,56 @@ export class AppService {
     orderBy?: Prisma.PinOrderByWithRelationInput;
   }): Promise<Pin[]> {
     const { skip, take, cursor, where, orderBy } = params;
-    return this.prisma.pin.findMany({
+
+    const randInt = Math.floor(Math.random() * 2);
+    if (randInt === 0) {
+      //try master first
+      try {
+        return this.master.pin.findMany({
+          skip,
+          take,
+          cursor,
+          where,
+          orderBy,
+        });
+      } catch (error) {
+        try {
+          return this.slave.pin.findMany({
+            skip,
+            take,
+            cursor,
+            where,
+            orderBy,
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    } else {
+      // try slave first
+      try {
+        return this.slave.pin.findMany({
+          skip,
+          take,
+          cursor,
+          where,
+          orderBy,
+        });
+      } catch (error) {
+        try {
+          return this.master.pin.findMany({
+            skip,
+            take,
+            cursor,
+            where,
+            orderBy,
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+    return this.master.pin.findMany({
       skip,
       take,
       cursor,
@@ -32,7 +114,18 @@ export class AppService {
   }
 
   async createPin(data: Prisma.PinCreateInput): Promise<Pin> {
-    return this.prisma.pin.create({ data });
+    let pin = null;
+    try {
+      pin = this.master.pin.create({ data });
+      this.slave.pin.create({ data });
+    } catch (error) {
+      try {
+        pin = this.slave.pin.create({ data });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    return pin;
   }
 
   async updatePin(params: {
@@ -40,10 +133,34 @@ export class AppService {
     data: Prisma.PinUpdateInput;
   }): Promise<Pin> {
     const { where, data } = params;
-    return this.prisma.pin.update({ where, data });
+    let pin = null;
+    try {
+      pin = this.master.pin.update({ where, data });
+      this.slave.pin.update({ where, data });
+    } catch (error) {
+      //master db is crashed, still have to call slave
+      try {
+        pin = this.slave.pin.update({ where, data });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    return pin;
   }
 
   async deletePin(where: Prisma.PinWhereUniqueInput): Promise<Pin> {
-    return this.prisma.pin.delete({ where });
+    let pin = null;
+    try {
+      pin = this.master.pin.delete({ where });
+      this.slave.pin.delete({ where });
+    } catch (error) {
+      //master db is crashed, still have to call slave
+      try {
+        pin = this.slave.pin.delete({ where });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    return pin;
   }
 }
